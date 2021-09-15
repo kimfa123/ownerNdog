461eedbc142de7e901bda246a7eeb453ae421002
JiHoon KIM <jihoonkim@JiHoonui-MacBookAir.local>
2021-09-15 15:15:30 +0900

    django ownerNdog

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 'rlawlgns123!@#',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-^6no*so&(u9h3wqmjj&f)8()g2=#!s6&s5_qecvgv^upcrlda-'
